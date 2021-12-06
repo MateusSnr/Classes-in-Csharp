7630003deb913f69499754308edff31cc35257d9
Mateus Salles <devmateussnr@gmail.com>
2021-12-06 19:42:43 -0300

    Update
    Code update

---
+++ PrimeirsExercicios/PessoasDados.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeirsExercicios
+{
+    class PessoasDados
+    {
+        public string nome;
+        public int idade;
+
+    }
+}
---
+++ PrimeirsExercicios/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PrimeirsExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoasDados Pessoa1, Pessoa2;
+
+            Pessoa1 = new PessoasDados();
+            Pessoa2 = new PessoasDados();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+
+            Console.Write("Nome: ");
+            Pessoa1.nome = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            Pessoa1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+
+            Console.Write("Nome: ");
+            Pessoa2.nome = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            Pessoa2.idade = int.Parse(Console.ReadLine());
+
+            if (Pessoa1.idade > Pessoa2.idade)
+            {
+                Console.WriteLine("\nPessoa mais velha: " + Pessoa1.nome);
+            }
+
+            else if (Pessoa1.idade == Pessoa2.idade)
+            {
+                Console.WriteLine("\nAmbas as pessoas possuem a mesma idade! ");
+            }
+
+            else
+            {
+                Console.WriteLine("\nPessoa mais velha: " + Pessoa2.nome);
+            }
+        }
+    }
+}
